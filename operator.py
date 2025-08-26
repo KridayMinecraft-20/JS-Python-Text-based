d039800eebc7973c622c2a245bfe4989af417ebe
KridayMinecraft-20 <apareek08@gmail.com>
2025-08-26 18:55:14 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,16 @@
+num1 = 45
+num2 = 3
+
+print("Division:", num1/num2)
+print("Floor division:", num1//num2)
+print("Division:", num1/num2)
+print("Modulus Operation:", num1%num2)
+print("square:", num1**num2)
+
+
+print("Equal?", num1==num2)
+print("Number 1 greater", num1>num2)
+print("Not Equal?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("Result of the equation is", result)
