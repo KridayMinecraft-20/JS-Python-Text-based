7774e0b4b1ba29ba7c00594a855cd66190b76366
KridayMinecraft-20 <apareek08@gmail.com>
2025-08-26 18:34:14 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,14 @@
+name = "kriday" #string;
+age = 10 #integer;
+is_student = True #boolean;
+weight = 38.5 #float
+
+
+
+print("Before type casting...")
+print(age)
+print("Data type of age is",type(age))
+print()
+newage = str(age)
+print("After type casting...")
+print("Data type of age is", type(newage))
