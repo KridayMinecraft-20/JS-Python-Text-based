37c19308849c81eb4bcaf6ad3baac5653f2aac16
KridayMinecraft-20 <apareek08@gmail.com>
2025-09-02 18:34:31 +0530

    Add files via upload

---
+++ sqaure.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+board = turtle.Turtle()
+
+#Square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
