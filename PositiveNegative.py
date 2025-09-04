ab611405f53d60f582fd7b76911648d537b2b5f9
KridayMinecraft-20 <apareek08@gmail.com>
2025-09-04 18:30:03 +0530

    Add files via upload
    Is it positive or negative? with a TWIST!

---
+++ PositiveNegative.py
@@ -0,0 +1,10 @@
+number = int(input("Enter The Number To Be Checked"))
+
+if number>0:
+ print("This is a positive number")
+
+elif number<0:
+ print("This is a negitive number")
+
+else:
+ print("This is a nuetral number") 
