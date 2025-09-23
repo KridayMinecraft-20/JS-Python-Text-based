f990bb138e36005340fa99e3f24a6872d54f2d62
KridayMinecraft-20 <apareek08@gmail.com>
2025-09-23 13:37:16 +0530

    Add files via upload

---
+++ Turtles.py
@@ -0,0 +1,39 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("Orange")
+
+pen = turtle.Turtle()
+pen.pensize(3)
+
+pen.penup()
+pen.goto(100,200)
+pen.pendown()
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+
+pen = turtle.Turtle()
+pen.pensize(3)
+
+pen.penup()
+pen.goto(-100,-100)
+pen.pendown()
+pen.forward(200)
+pen.left(120)
+pen.forward(200)
+pen.left(120)
+pen.forward(200)
+pen.left(120)
+
+
+
+
+ 
+
+ 
