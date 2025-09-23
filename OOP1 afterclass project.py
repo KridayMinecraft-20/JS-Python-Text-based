c692ce95f7576e6691bf3ffaea271a8e38a4f1e6
KridayMinecraft-20 <apareek08@gmail.com>
2025-09-23 12:56:49 +0530

    Add files via upload

---
+++ OOP1 afterclass project.py
@@ -0,0 +1,10 @@
+class Human:
+   type = "enthusiastic, hyperactive"
+def __init__(self, name, age):
+   self.name = name
+   self.age = age
+
+Kriday = Human("Kriday", 10)
+
+print("I am  {}" .format(type))
+print("My name is {} and I am {} y/o" (Kriday.name, Kriday.age))
