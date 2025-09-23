ca490225074c482662e44a03287b93e78e16483a
KridayMinecraft-20 <apareek08@gmail.com>
2025-09-23 13:23:04 +0530

    Add files via upload

---
+++ Function afterclass.py
@@ -0,0 +1,14 @@
+# Ask the user to enter two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+
+def find_sum(a, b):
+    return a + b
+
+
+def find_product(a, b):
+    return a * b
+
+print("The sum of the two numbers is:", find_sum)
+print("The product of the two numbers is:", find_product)
